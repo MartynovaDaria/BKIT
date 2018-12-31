5eb5f04bf8a5544f6fd5a20e2b1800782b355082
Martynova Daria <39467141+MartynovaDaria@users.noreply.github.com>
2019-01-01 01:17:34 +0300

    Update Circle.cs

--- LW2/LW2/Figure/Circle.cs
+++ LW2/LW2/Figure/Circle.cs
@@ -6,82 +6,22 @@ using System.Threading.Tasks;
 
 namespace LW2
 {
-    class Program
+    
+    class Circle : GeometryFigure
     {
-        static void Main(string[] args)
+        public Circle(double radius)
         {
-            Rectangle a = new Rectangle(10,20);
-            Console.WriteLine(a.Print());
+            this.radius = radius;
+        }
+        public double radius { get; set; }
 
-            Square b = new Square(4);
-            Console.WriteLine(b.Print());
-
-            Circle c = new Circle(6);
-            Console.WriteLine(c.Print());  
-            
-            Console.ReadLine();
+        public override double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+        public override string ToString()
+        {
+            return "Круг с радиусом " + radius +" и площадью " + Area();
         }
     }
-}
-
-abstract class GeometryFigure: IPrint
-{
-    public abstract double Area();
-
-    public string Print()
-    {
-        return ToString();
-    }
-}
-
-class Rectangle : GeometryFigure
-{
-    public Rectangle(double width,double length)
-    {
-        this.width = width;
-        this.length = length;
-    }
-    public double width { get; set; }
-    public double length { get; set; }
-
-    public override double Area()
-    {
-        return  length * width;
-    }
-    public override string ToString()
-    {
-        return "Прямоугольник с длиной " + width + ", шириной " + length + " и площадью " + Area();
-    }
-}
-
-class Square : Rectangle
-{
-    public Square(double width): base(width, width){}
-    public override string ToString()
-    {
-        return "Квадрат со стороной " + width + " и площадью " + Area();
-    }
-}
-
-class Circle : GeometryFigure
-{
-    public Circle(double radius)
-    {
-        this.radius = radius;
-    }
-    public double radius { get; set; }
-
-    public override double Area()
-    {
-        return Math.PI * radius * radius;
-    }
-    public override string ToString()
-    {
-        return "Круг с радиусом " + radius +" и площадью " + Area();
-    }
-}
-
-interface IPrint
-{
-    string Print();
-}
+}       
